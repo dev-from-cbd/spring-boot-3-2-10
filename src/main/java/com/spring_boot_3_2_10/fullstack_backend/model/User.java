af656b44589ab42755592b7deb904ab22b06f208
dev-from-cbd <veryfatwombat@gmail.com>
2024-10-15 07:09:17 +1030

    Full Stack Spring Boot and React CRUD

--- src/main/java/com/spring_boot_3_2_10/fullstack_backend/model/User.java
+++ src/main/java/com/spring_boot_3_2_10/fullstack_backend/model/User.java
@@ -1,47 +1,55 @@
-package com.spring_boot_3_2_10.fullstack_backend.model;
+package com.spring_boot_3_2_10.fullstack_backend.model; // Define the package for this class
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.Entity; // Import Entity annotation for marking this class as a JPA entity
+import jakarta.persistence.GeneratedValue; // Import GeneratedValue annotation for auto-generating primary key values
+import jakarta.persistence.Id; // Import Id annotation to mark the primary key field
 
-@Entity
+@Entity // Mark this class as a JPA entity that will be mapped to a database table
 public class User {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String username;
-    private String name;
-    private String email;
+    @Id // Mark this field as the primary key
+    @GeneratedValue // Auto-generate the value for this primary key
+    private Long id; // Unique identifier for each User
+    private String username; // Username of the user
+    private String name; // Name of the user
+    private String email; // Email address of the user
 
+    // Getter for id field
     public Long getId() {
         return id;
     }
 
+    // Setter for id field
     public void setId(Long id) {
         this.id = id;
     }
 
+    // Getter for username field
     public String getUsername() {
         return username;
     }
 
+    // Setter for username field
     public void setUsername(String username) {
         this.username = username;
     }
 
+    // Getter for name field
     public String getName() {
         return name;
     }
 
+    // Setter for name field
     public void setName(String name) {
         this.name = name;
     }
 
+    // Getter for email field
     public String getEmail() {
         return email;
     }
 
+    // Setter for email field
     public void setEmail(String email) {
         this.email = email;
     }
--- src/main/java/com/spring_boot_3_2_10/fullstack_backend/repository/UserRepository.java
+++ src/main/java/com/spring_boot_3_2_10/fullstack_backend/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.spring_boot_3_2_10.fullstack_backend.repository;
 
-public interface UserRepository {
+import com.spring_boot_3_2_10.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User> {
+
 }
