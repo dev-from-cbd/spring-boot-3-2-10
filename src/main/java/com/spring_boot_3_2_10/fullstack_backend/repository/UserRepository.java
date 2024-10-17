50431da4397fd03f56cfe85839f963474fef976c
dev-from-cbd <veryfatwombat@gmail.com>
2024-10-18 08:32:44 +1030

    Java

--- src/main/java/com/spring_boot_3_2_10/fullstack_backend/repository/UserRepository.java
+++ src/main/java/com/spring_boot_3_2_10/fullstack_backend/repository/UserRepository.java
@@ -3,6 +3,5 @@ package com.spring_boot_3_2_10.fullstack_backend.repository;
 import com.spring_boot_3_2_10.fullstack_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User> {
-
+public interface UserRepository extends JpaRepository<User,Long> {
 }
